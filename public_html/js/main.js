8edd0c0aee433d0961647c1110c3dd6ce86bce9a
Milan Grubor <milangrubor192@gmail.com>
2023-08-30 21:31:33 +0200

    initial commit

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     
+    if($(".team-members-slider").length>0){
+         $(".team-members-slider").owlCarousel({
+             
+             loop:true,
+             autoplay:true,
+             responsive:{
+                 0:{
+                     items:1,
+                     margin:0
+                 }
+                
+             }
+             
+         });
+    }
+    
     
     
     
